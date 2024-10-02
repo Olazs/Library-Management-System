aa0347da87163a1004ded569f6dc3c84dcff324e
Gál Olivér Balázs <oliverbalazsgal@outlook.com>
2024-10-02 19:25:14 +0200

    -App.c created. -The struct of library has been created.

---
+++ App.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct library{
+    char title[32];
+    char author[32];
+    int pages;
+    float price;
+};
+
+int main(){
+    return 0;
+}
