688fbd5bcff2152b6d340b67575c92ddeafc752e
Gál Olivér Balázs <70948378+Olazs@users.noreply.github.com>
2024-10-03 09:02:50 +0200

    Finished on lab.
    Total duration of the code: 1:12

--- App.c
+++ App.c
@@ -19,30 +19,59 @@ int main(){
 
         printf("\n\n********######Udvozollek a konyvtarban!######********\n");
         printf("\n1. Konyv Hozzaadasa\n");
-        printf("\n2. Konyv Lekerdezese\n");
+        printf("\n2. Konyvek listazasa\n");
         printf("\n3. Egy iro munkassaganak lekerdezese\n");
         printf("\n4. Konyvek szamanak lekerdezese a konyvtarbol\n");
         printf("\n5. Kilepes\n");
 
-    }
+    
     printf("\n\n Kerlek adj meg egyet a fentiek kozul!");
     scanf("%d", &operator);
+    
     switch (operator) //Menu kezelo
     {
     case 1:
-    printf("Add meg a konyv nevet!: ");
-    scanf("%s", lib[i].title);
-    printf("Add meg a szerzo nevet!: ");
-    scanf("%s", lib[i].author);
-    printf("Add meg a konyv oldalainak a szamat!: ");
-    scanf("%d", lib[i].pages);
-    printf("Add meg a konyv arat!: ");
-    scanf("%f", lib[i].author);
-        break;
-    
-    default:
-        break;
+    	printf("Add meg a konyv nevet!: ");
+    	scanf("%s", lib[i].title);
+    	printf("Add meg a szerzo nevet!: ");
+    	scanf("%s", lib[i].author);
+    	printf("Add meg a konyv oldalainak a szamat!: ");
+    	scanf("%d", &lib[i].pages);
+    	printf("Add meg a konyv arat!: ");
+    	scanf("%2f", &lib[i].price);
+    	i++;
+    	count++;
+    break;
+        
+    case 2:
+    	printf("\nAz alabbi konyvek talalhatoak a konyvtarban: \n");
+    	for(i=0; i<count; i++){ //SizeOf?/Count? 
+    		printf("Konyvcim: %s", lib[i].title);
+    		printf("\tSzerzo: %s", lib[i].author);
+    		printf("\tOldalszam: %d", lib[i].pages);
+    		printf("\tAr: %f", lib[i].price);
+    		printf("\n");
+		}
+	break;
+	
+	case 3: 
+		printf("Add meg a szerzo nevet!: ");
+		scanf("%s", author_name);
+		for(i=0; i<count; i++){ //SizeOf?/Count?
+			if (strcmp(author_name, lib[i].author)==0){
+				printf("%s %s %d %f", lib[i].title, lib[i].author, lib[i].pages, lib[i].price);
+			}
+		}
+	break;
+	
+	case 4: 
+		printf("\n Konyvek szama a konyvtarban: %d", count); //SizeOf?/Count? 
+	break;
+	
+	case 5:
+		exit(0);
     }
+}
 
     return 0;
 }
